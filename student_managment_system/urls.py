1b671f9f690b0a6b90ee1ed10e96b818a3f2db5b
eli <ebezhiasvhili@gmail.com>
2024-09-23 16:45:41 +0400

    add new student view

--- student_managment_system/urls.py
+++ student_managment_system/urls.py
@@ -4,4 +4,5 @@ from . import  views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.view_student, name='view_student'),
+    path('add_student/', views.add_student, name='add_student'),
 ]
--- student_managment_system/views.py
+++ student_managment_system/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from student_managment_system.models import Student
 
@@ -12,3 +13,17 @@ def view_student(request, id):
     student = Student.objects.get(pk=id)
     context = {'student': student}
     return render(request, 'students/student_details.html', context)
+
+
+def add_student(request):
+    if request.method == 'POST':
+       Student.objects.create(
+           student_number = request.POST['student_number'],
+           first_name=request.POST['first_name'],
+           last_name=request.POST['last_name'],
+           email = request.POST['email'],
+           field_of_study=request.POST['field_of_study'],
+           gpa=request.POST['gpa'],
+       )
+       return HttpResponse('Student added successfully')
+    return render(request, 'students/add_student.html')
